a1044f453aa2173c8d6d8019aa33b51d8bcd861b
Maanas Krishna VSL <vslk.maanas@gmail.com>
2025-02-16 14:47:06 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ import pandas as pd
 import matplotlib.pyplot as plr
 import seaborn as sns
 
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
 
 
@@ -21,3 +23,26 @@ def save_object(file_path , obj):
 
     except Exception as e:
         raise CustomException(e , sys)
+
+
+def evaluate_models(X_train , y_train , X_test , y_test , models):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]  # Accessing a model
+
+            model.fit(X_train , y_train) # Train a model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train , y_train_pred)
+
+            test_model_score = r2_score(y_test , y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    except:
+        pass
